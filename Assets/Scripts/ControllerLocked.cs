a9a81df093a1d06e1b70ab8eb598c4e25ed82a82
Nathan Wolf <nathan@elmakers.com>
2018-10-28 09:56:31 -0700

    Add a controller-locked wand model

--- Assets/Scripts/ControllerLocked.cs
+++ Assets/Scripts/ControllerLocked.cs
@@ -1,16 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using MagicKit;
 using UnityEngine;
 
-public class ControllerLocked : MonoBehaviour {
+public class ControllerLocked : MonoBehaviour
+{
+	private ControllerInput _input;
+	private Rigidbody _body;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		_input = GetComponent<ControllerInput>();
+		_body = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		// _body.transform.position = Camera.main.transform.position + Camera.main.transform.forward * GrabDistance;
+
+		_body.transform.position = _input.position;
+		_body.transform.rotation = _input.orientation;
 	}
 }
