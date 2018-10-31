7db75f02304514ff4766b2e2b593d9b71065d586
Nathan Wolf <nathan@elmakers.com>
2018-10-31 07:30:38 -0700

    Add a projectile and explosion

---
+++ Assets/Scripts/LaunchProjectile.cs
@@ -0,0 +1,24 @@
+﻿using MagicKit;
+using UnityEngine;
+
+public class LaunchProjectile : MonoBehaviour {
+	public GameObject Projectile;
+	public GameObject SpawnLocation;
+	public float Speed = 10.0f;
+	
+	private ControllerInput _input;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_input = GetComponent<ControllerInput>();
+		_input.OnTriggerDown += OnTriggerDown;
+	}
+
+	void OnTriggerDown()
+	{
+		GameObject spawned = Instantiate(Projectile, _input.position, Quaternion.identity);
+		spawned.GetComponent<Rigidbody>().AddForce(SpawnLocation.transform.forward * Speed);
+		Debug.Log("LAUNCH from " + _input.position);
+	}
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,36 @@
+﻿using MagicKit;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+	public GameObject ParticleSystem;
+	public GameObject Explosion;
+	public float Lifespan = 10.0f;
+
+	private ControllerInput _input;
+
+	// Use this for initialization
+	void Awake()
+	{
+		Destroy(gameObject, Lifespan);
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		Debug.Log("Collided!");
+		// Spawn an explosion
+		if (Explosion)
+		{
+			Instantiate(Explosion, transform.position, Quaternion.identity);
+		}
+		
+		// Stop the particle trail
+		var particles = ParticleSystem.GetComponent<ParticleSystem>();
+		particles.Stop();
+
+		// Unparent the particles, give them a chance to finish
+		particles.transform.parent = null;
+		Destroy(particles.gameObject, particles.main.duration);
+		Destroy(gameObject);
+	}
+}
